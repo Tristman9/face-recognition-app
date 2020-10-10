1bc466e1588fc8dd496d8b565d17917b34133f50
Tristan Dobos <tristandobos@gmail.com>
2020-10-10 15:31:33 +0200

    Updates

---
+++ precache-manifest.4edfc99b160f6ff997c71b3ee4b66885.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4a9326e4f409fa5cc35335c2b9e66a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a21ac3493f3d35143609",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3a1a39fc8decb2073e79",
+    "url": "/static/css/main.0b95c73b.chunk.css"
+  },
+  {
+    "revision": "a21ac3493f3d35143609",
+    "url": "/static/js/2.c86c1f5c.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.c86c1f5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a1a39fc8decb2073e79",
+    "url": "/static/js/main.3e522081.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
